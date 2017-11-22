8e7bc0f7cb76c67a6e7671e1253864c93d52149b
Talha Rehman <14beetrehman@seecs.edu.pk>
2017-11-22 11:29:17 +0500

    dev: adds tp, tn, fp, fn to evaluation

--- eval.py
+++ eval.py
@@ -6,6 +6,11 @@ total = 0
 correct = 0
 count=0
 
+fp=0
+fn=0
+tp=0
+tn=0
+
 for test_file in os.listdir('TestData'):
 	count=count+1
 	if test_file.endswith('.csv'):
@@ -38,14 +43,32 @@ for test_file in os.listdir('TestData'):
 		gt = gt_data[i]
 		total += 1
 		tot += 1
-		if test[2] == gt[2]:
+		
+		if len(test[1]) !=0 and len(gt[1])==0:
+			fp += 1
+
+		if len(test[1]) ==0 and len(gt[1]) !=0:
+			fn += 1
+		
+		if len(test[1]) ==0 and len(gt[1])==0:
+			tn += 1
+
+		if len(test[1]) !=0 and len(gt[1]) !=0:
+			tp += 1
+				
+
+
+		if test[2] == gt[2] and test[1] == gt[1]:
 			correct += 1
 			cor += 1
 	print('\tAccuracy: ', 100*cor/tot)
-print('Final Accuray: ', 100*correct/total)
-print "Total Files: ",count/2
 
-print correct
-print total
+print "\n ANALYTICS"
+print "Total Files: ",count/2
+print'Final Accuray: ', 100*correct/total
+print "True Negative: ",100*tn/total
+print "True positive ",100*tp/total
+print "False Negative: ",100*fn/total
+print "False positive ",100*fp/total
 
 	
