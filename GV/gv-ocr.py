47723118f6626221341bff7bf5fbafa2a02d72a7
Talha Rehman <14beetrehman@seecs.edu.pk>
2018-04-07 15:21:52 +0500

    dev: generates initial json

--- GV/gv-ocr.py
+++ GV/gv-ocr.py
@@ -3,6 +3,7 @@
 import argparse
 import io
 import sys
+import json
 
 from google.cloud import vision
 
@@ -19,11 +20,35 @@ def detect_text(path):
     response = client.text_detection(image=image)
     texts = response.text_annotations
 
+    data={}
+    data["sentences"]=[]
+
     for text in texts:
         ocr_output='\n"{}"'.format(text.description.encode('utf-8'))
         break
     
 
+
     lines = ocr_output.split('\n')
+    count=0
+    for line in lines:
+        thisline={}
+        thisline["sentenceID"]=count
+        thisline["sentence"]=line
+        thisline["words"]=[]
+        words=line.split(' ')
+        
+        for word in words:
+            thisword={}
+            thisword["word"]=word
+            thisword["boundingbox"]={"top":'',"left":'',"bottom":'',"right":'',}
+            thisline["words"].append(thisword)
+
+
+        data["sentences"].append(thisline)
+        count=count+1
+    
+    json.dump(data, open('ocr.json', 'wb'))
+
 
 detect_text(sys.argv[1])
